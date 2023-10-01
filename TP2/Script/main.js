0faf77cee11591d5cebaf265da722aa1cc73dcad
LucasCarmusciano <lucascarmusciano@gmail.com>
2023-10-01 15:46:10 -0300

    realizo segundo carrusel

--- TP2/Script/main.js
+++ TP2/Script/main.js
@@ -45,18 +45,78 @@ function updateCarouselPosition() {
 }
 
 function checkBoundary() {
-    const cardWidth = card.offsetWidth;
-    console.log(cardWidth);
-    const carouselWidth = carousel.offsetWidth;
-    console.log(carouselWidth);
-    const maxTranslate = carouselWidth;
-    const minTranslate = -carouselWidth;
-    if (currentTranslate > maxTranslate) {
-      currentTranslate = maxTranslate;
-    } else if (currentTranslate < minTranslate) {
-      currentTranslate = minTranslate;
-    }
-  
-    updateCarouselPosition();
+  const cardWidth = card.offsetWidth;
+  const carouselWidth = carousel.offsetWidth;
+  const maxTranslate = cardWidth/16; // Limita el desplazamiento a 0 (posición inicial)
+  const minTranslate = -carouselWidth*(2/3) + cardWidth; // Evita que se desplace más allá del último elemento
+
+  if (currentTranslate > maxTranslate) {
+    currentTranslate = maxTranslate;
+  } else if (currentTranslate < minTranslate) {
+    currentTranslate = minTranslate;
   }
-  
+
+  updateCarouselPosition();
+}
+
+
+
+
+// Carrusel 2
+const carousel2 = document.querySelector('.carousel-main-container');
+const card2 = document.querySelector('.carousel-item-2');
+let isDragging2 = false;
+let startPosition2 = 0;
+let currentTranslate2 = 0;
+let prevTranslate2 = 0;
+
+carousel2.addEventListener('mousedown', (e) => {
+  if (!isDragging2) {
+    isDragging2 = true;
+    startPosition2 = e.clientX;
+    prevTranslate2 = currentTranslate2;
+    carousel2.style.cursor = 'grabbing';
+  }
+});
+
+carousel2.addEventListener('mousemove', (e) => {
+  if (!isDragging2) return;
+  const currentPosition2 = e.clientX;
+  currentTranslate2 = prevTranslate2 + currentPosition2 - startPosition2;
+  updateCarouselPosition2();
+});
+
+carousel2.addEventListener('mouseup', () => {
+  if (isDragging2) {
+    isDragging2 = false;
+    carousel2.style.cursor = 'grab';
+    checkBoundary2();
+  }
+});
+
+carousel2.addEventListener('mouseleave', () => {
+  if (isDragging2) {
+    isDragging2 = false;
+    carousel2.style.cursor = 'grab';
+    checkBoundary2();
+  }
+});
+
+function updateCarouselPosition2() {
+  carousel2.style.transform = `translateX(${currentTranslate2}px)`;
+}
+
+function checkBoundary2() {
+  const cardWidth2 = card2.offsetWidth;
+  const carouselWidth2 = carousel2.offsetWidth;
+  const maxTranslate2 = 0; // Limita el desplazamiento a 0 (posición inicial)
+  const minTranslate2 = -carouselWidth2 * (2 / 3) + cardWidth2; // Evita que se desplace más allá del último elemento
+
+  if (currentTranslate2 > maxTranslate2) {
+    currentTranslate2 = maxTranslate2;
+  } else if (currentTranslate2 < minTranslate2) {
+    currentTranslate2 = minTranslate2;
+  }
+
+  updateCarouselPosition2();
+}
