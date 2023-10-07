c83d49ab94bed5e6d255570f97eb6bf2a99ad494
Agustin Carmusciano <agustincarmu99@gmail.com>
2023-10-07 01:01:31 -0300

    push

--- TP2/Script/juego.js
+++ TP2/Script/juego.js
@@ -1,7 +1,22 @@
-var textarea = document.querySelector('textarea[name="comentario"]');
-var divContenedor = document.querySelector('.contenido-comentario');
+"use strict";
+
+const textarea = document.querySelector('textarea[name="comentario"]');
+const divContenedor = document.querySelector('.comentar');
+const boton = document.querySelector('.boton-comentar');
+
 
 textarea.addEventListener("input", function() {
-    console.log(textarea.clientHeight);
-    
+    boton.classList.add("activado");
+    if(textarea.value === "") {
+        boton.classList.remove("activado");
+    }
+});
+
+textarea.addEventListener("focus", function() {
+    divContenedor.classList.add("activado");
+}
+);
+
+textarea.addEventListener("blur", function() {
+    divContenedor.classList.remove("activado");
 });
